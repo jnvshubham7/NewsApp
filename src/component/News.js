2713ee2de829d281437fb61302aed85cd76bc54e
Shubham Kumar Bhokta <69629803+jnvshubham7@users.noreply.github.com>
2024-01-31 18:10:35 +0530

    update

---
+++ src/component/News.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+ import NewsItem from './NewsItem'
+export class News extends Component {
+  render() {
+    return (
+      <div>
+
+        this is a news componeent 
+
+       <NewsItem/>
+       <NewsItem/>
+       <NewsItem/>
+       <NewsItem/>
+       <NewsItem/>
+       
+       
+
+
+
+       
+
+     
+
+
+       
+
+       
+
+       
+
+        
+      </div>
+    )
+  }
+}
+
+export default News
---
+++ src/component/NewsItem.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+  render() {
+    return (
+      <div>
+
+        this is news item 
+        
+      </div>
+    )
+  }
+}
+
+export default NewsItem
